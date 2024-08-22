d908f03dbf76d5498d33898564eac95eed797270
malvaradoadip <114782291+malvaradoadip@users.noreply.github.com>
2024-08-22 13:16:12 -0600

    shopping list + verse selector 2

--- javascript/asynchronous/sequencing-animations/start/main.js
+++ javascript/asynchronous/sequencing-animations/start/main.js
@@ -12,3 +12,28 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+//secuencia(alice1.animate(aliceTumbling, aliceTiming))
+//alice1.animate(aliceTumbling, aliceTiming);
+//alice2.animate(aliceTumbling, aliceTiming);
+//alice3.animate(aliceTumbling, aliceTiming);
+//function secuencia1(mycallback){
+//  alice1.animate(aliceTumbling,aliceTiming)
+//}
+
+
+
+function secuencia(mycallback){
+  alice1.animate(aliceTumbling,aliceTiming)
+//  mycallback(aliceTumbling,aliceTiming);
+}
+function secuencia2(aliceTumbling,aliceTiming,mycallback){
+  mycallback(aliceTumbling,aliceTiming);
+}
+function secuencia3(aliceTumbling,aliceTiming,mycallback){
+    mycallback(aliceTumbling,aliceTiming);
+  }
+function completo(mycallback){
+  mycallback(aliceTumbling,aliceTiming)
+}
+completo(secuencia)
